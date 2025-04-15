430f270c17f868f6b34cb9c24c8f38fa46d24d61
zeidexe <zzeid.exe@gmail.com>
2025-04-15 16:15:29 +0300

    Initial commit to main

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Hotels Jordan | Your Guide to Jordan's Best Accommodations",
   description: "Explore Jordan's hotels, destinations, and activities with our comprehensive travel guide.",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -22,6 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-adsense-account" content="ca-pub-7142894016772610" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7142894016772610"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col">
@@ -33,6 +37,3 @@ export default function RootLayout({
     </html>
   )
 }
-
-
-import './globals.css'
