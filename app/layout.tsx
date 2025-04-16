b6b466fad5ef5c3a230fbdd7e86a390ab71d782e
zeidexe <zzeid.exe@gmail.com>
2025-04-16 14:43:09 +0300

    Initial commit from blog final projesssssssct

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Hotels Jordan | Your Guide to Jordan's Best Accommodations",
+  description: "Explore Jordan's hotels, destinations, and activities with our comprehensive travel guide.",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-7142894016772610" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7142894016772610"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  )
+}
+
+
+import './globals.css'
